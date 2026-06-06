0fdcee1dd3c7a83a27b0599c6884d4beb8de3a8f
miho-imakawa <mihou.imakawa@gmail.com>
2026-06-07 02:27:09 +0900

    update FM again

--- frontend-sns/src/components/FriendManager.tsx
+++ frontend-sns/src/components/FriendManager.tsx
@@ -366,7 +366,7 @@ const FriendManager: React.FC = () => {
 
     return (
         <div className="p-6">
-            <h1 className="text-2xl font-bold mb-4">FRIENDs management</h1>
+            <h1 className="text-2xl font-bold mb-4">FRIENDs' management</h1>
 
             {/* SetupIntent 完了後メッセージ */}
             {isProcessingSetup && (
